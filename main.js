b51874a4169cd6c4b7ccb8bdb3040a01d9a6c00d
Luis Carlos Dominguez del Angel <luisdgza96@gmail.com>
2021-02-06 11:37:14 -0600

    replace files

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-const app = new Vue({
-    el: '#app',    
-    data () {
-        return {
-          info: null
+const $pokemon = document.querySelector('#pokemon')
+
+function renderPokemon(image){
+    $pokemon.setAttribute('src',image)
+}
+
+
+
+
+
+$(document).on('click', '.btn-detalles', function(event)
+{
+    event.preventDefault();
+    let id = $(this).data('id');
+    let url = 'https://pokeapi.co/api/v2/pokemon/'+id;
+    const app = new Vue({
+        el: '#app',    
+        data () {
+            return {
+              info: null
+            }
+          },
+        mounted () {
+        axios
+            .get(url)
+            .then(response => (renderPokemon(response.data.sprites.front_default) ))
         }
-      },
-    mounted () {
-    axios
-        .get('https://pokeapi.co/api/v2/pokemon/25')
-        .then(response => (this.info = response.data.sprites.back_default))
-    }
-})
+    })
+    
+});
+
+$(document).on('click', '.btn-elegir', function(event)
+{
+    event.preventDefault();
+    let tipo = $(this).data('tipo');
+    $('#h-eleccion').html("Usted escogió: "+tipo);
+    console.log(tipo);
+    
+});
